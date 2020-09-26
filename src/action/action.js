30bc8df6a6d8f436f0468f0df71ce43e60e31a8c
suresharam <suriram.sam@gmail.com>
2020-09-26 13:52:16 +0530

    Initial code from create-react-app cli

--- src/action/action.js
+++ src/action/action.js
@@ -10,7 +10,7 @@ export const loadNews = (values) => {
 export const call_news_api = () =>
 {
     return(dispatch) => {
-        axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1c7fce68ee7a4cdcb395a5e02cca3aa1`)
+        axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=99b341165d6f4d5fa51db70d3080468b`)
         .then(response =>{
             const loadResults = response.data;
             dispatch(loadNews(loadResults))
@@ -23,7 +23,7 @@ export const call_news_api = () =>
 export const call_filter_news_api = (param) =>
 {
     return(dispatch) => {
-        axios.get(`https://newsapi.org/v2/everything?q=${param}&apiKey=1c7fce68ee7a4cdcb395a5e02cca3aa1`)
+        axios.get(`https://newsapi.org/v2/everything?q=${param}&apiKey=99b341165d6f4d5fa51db70d3080468b`)
         .then(response =>{
             const filterResults = response.data;
             dispatch(loadNews(filterResults))
